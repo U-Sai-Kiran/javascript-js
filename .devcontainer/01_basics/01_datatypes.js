4447d7244c85c7c4d13008ecee3f5027f6f8928c
U-Sai-Kiran <145639746+U-Sai-Kiran@users.noreply.github.com>
2024-07-24 18:00:30 +0000

    container congif done

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use script" // treats as a new version of js
+
+//alert(hello) // we are using node js not browser
+
+let id = 123
+let name = "sai"
+let loggedin = false
+let state;
+console.log(id)
+
+/* number = 2 to power 53
+bigint
+string = ""
+boolean = true/false
+null = standalone value/object
+undefined
+symbols = unique */
+
+// object
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountid = 12345
+let accountmail = "123.com"
+var accountname = "sai"
+accountpaswd = "sai123"
+let accountstate
+
+// accountid = 54321
+console.log(accountid);
+console.table([accountid,accountmail,accountname,accountpaswd,accountstate])
